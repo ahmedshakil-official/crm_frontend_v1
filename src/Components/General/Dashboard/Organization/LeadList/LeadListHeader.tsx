92dbf248e1d13989997b3805e6a0069d77345db7
Md Mahbub Rahman <54384173+mahbub045@users.noreply.github.com>
2024-11-22 18:26:17 +0600

    Update LeadListHeader.tsx

--- src/Components/General/Dashboard/Organization/LeadList/LeadListHeader.tsx
+++ src/Components/General/Dashboard/Organization/LeadList/LeadListHeader.tsx
@@ -1,13 +1,11 @@
-import React from 'react'
-
 const LeadListHeader = () => {
   return (
     <>
       <div className="pt-3 px-3">
-        <p>Lists of Lead in my Organization</p>
+        <p>List of Leads in my Organization</p>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default LeadListHeader
+export default LeadListHeader;
