1347ca40b3a5c0d91d5f2c96f490ef7a5023d29e
Md Mahbub Rahman <54384173+mahbub045@users.noreply.github.com>
2024-11-25 17:00:47 +0600

    Change delete modal name color

--- src/Components/General/Dashboard/Organization/EmployeeList/EmployeeListBody.tsx
+++ src/Components/General/Dashboard/Organization/EmployeeList/EmployeeListBody.tsx
@@ -578,7 +578,7 @@ const EmployeeListBody: React.FC = () => {
       <Modal isOpen={deleteModal} toggle={toggleDeleteModal}>
         <ModalHeader toggle={toggleDeleteModal}>Confirm Delete</ModalHeader>
         <ModalBody>
-          Are you sure you want to delete <strong>{employeeToDelete}</strong>?
+          Are you sure you want to delete <strong className="text-danger">{employeeToDelete}</strong>?
         </ModalBody>
         <ModalFooter>
           <Button color="danger" onClick={confirmDelete}>
--- src/Components/General/Dashboard/Organization/IntroducerList/IntroducerListBody.tsx
+++ src/Components/General/Dashboard/Organization/IntroducerList/IntroducerListBody.tsx
@@ -507,7 +507,7 @@ const IntroducerListBody: React.FC = () => {
       <Modal isOpen={deleteModal} toggle={toggleDeleteModal}>
         <ModalHeader toggle={toggleDeleteModal}>Confirm Delete</ModalHeader>
         <ModalBody>
-          Are you sure you want to delete <strong>{introducerToDelete}</strong>?
+          Are you sure you want to delete <strong className="text-danger">{introducerToDelete}</strong>?
         </ModalBody>
         <ModalFooter>
           <Button color="danger" onClick={confirmDelete}>
