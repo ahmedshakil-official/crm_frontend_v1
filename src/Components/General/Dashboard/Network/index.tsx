a6e6eea282ab30fa53df71606cee442c7a096d12
Md Mahbub Rahman <54384173+mahbub045@users.noreply.github.com>
2024-12-24 18:39:54 +0600

    Load Organization dashboard data

--- src/Components/General/Dashboard/Network/index.tsx
+++ src/Components/General/Dashboard/Network/index.tsx
@@ -1,15 +1,38 @@
 import { Container, Row } from "reactstrap";
 import NetworkBreadcrumbs from "./components/Breadcrumbs";
 import OrganizationCards from "./components/Organizations/OrganizationCards";
+import { useState } from "react";
+import ActivityStatus from "../Organization/ActivityStatus/ActivityStatus";
+import LeadList from "../Organization/LeadList/LeadList";
+import ClientList from "../Organization/ClientList/ClientList";
+import AdvisorList from "../Organization/AdvisorList/AdvisorList";
+import IntroducerList from "../Organization/IntroducerList/IntroducerList";
+
 
 
 const ContainerNetwork = () => {
+  const [isFetchedLead, setIsFetchedLead] = useState(false);
   return (
     <>
       <NetworkBreadcrumbs />
       <Container fluid className="default-dashboard">
         <Row>
           <OrganizationCards />
+        </Row>
+        <Row>
+          <ActivityStatus isFetchedLead={isFetchedLead}  setIsFetchedLead={setIsFetchedLead}/>
+        </Row>
+        <Row>
+          <LeadList setIsFetchedLead={setIsFetchedLead} />
+        </Row>
+        <Row>
+          <ClientList />
+        </Row>
+        <Row>
+          <AdvisorList />
+        </Row>
+        <Row>
+          <IntroducerList />
         </Row>
       </Container>
     </>
