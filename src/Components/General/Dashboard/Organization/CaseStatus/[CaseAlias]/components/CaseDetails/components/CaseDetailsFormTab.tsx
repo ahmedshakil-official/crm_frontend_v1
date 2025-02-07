6bb68ce1f6fe65cd4b061f33b604dc489239d019
Shazzdul-Shakib <shakib1186@gmail.com>
2025-02-07 16:40:01 +0600

    conditional renderinf of tab

--- src/Components/General/Dashboard/Organization/CaseStatus/[CaseAlias]/components/CaseDetails/components/CaseDetailsFormTab.tsx
+++ src/Components/General/Dashboard/Organization/CaseStatus/[CaseAlias]/components/CaseDetails/components/CaseDetailsFormTab.tsx
@@ -3,32 +3,51 @@ import { useState } from "react";
 import { Card, CardBody, Col, Nav, NavItem, NavLink } from "reactstrap";
 import { CaseDetailsFormTabContent } from "./CaseDetailsFormTabContent";
 import { CaseDetailsFormTabTitleData } from "@/Data/Case/CaseDetails/CaseDetailsFormTabTitleData";
+import { useAppSelector } from "@/Redux/Hooks";
+import { LoanDetailsFormFields } from "@/Data/Case/CaseDetails/LoanDetails/LoanDetailsFormData";
 
 export const CaseDetailsFormTab = () => {
   const [basicTab, setBasicTab] = useState("1");
+  const value = useAppSelector((state) => state.caseDetails.basicTabId);
+  const fields = LoanDetailsFormFields;
 
   return (
     <Col xxl="12">
       <Card>
         <CardBody className="text-center">
-          <Nav tabs className="border-tab mb-0 d-flex justify-content-center">
-            {CaseDetailsFormTabTitleData[0].map((item) => (
-              <NavItem key={item.id}>
-                <NavLink
-                  href={Href}
-                  className={`nav-border text-info tab-info ${
-                    item.id === "1" ? "pt-0" : ""
-                  } ${basicTab === item.id ? "active" : ""}`}
-                  onClick={() => setBasicTab(item.id)}
-                >
-                  {item.text}
-                </NavLink>
-              </NavItem>
-            ))}
-          </Nav>
-          <div className=" w-75 mx-auto">
-            <CaseDetailsFormTabContent tabId={basicTab} />
-          </div>
+          {value === "1" ? (
+            <>
+              <Nav
+                tabs
+                className="border-tab mb-0 d-flex justify-content-center"
+              >
+                {CaseDetailsFormTabTitleData[0].map((item) => (
+                  <NavItem key={item.id}>
+                    <NavLink
+                      href={Href}
+                      className={`nav-border text-info tab-info ${
+                        item.id === "1" ? "pt-0" : ""
+                      } ${basicTab === item.id ? "active" : ""}`}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setBasicTab(item.id);
+                      }}
+                    >
+                      {item.text}
+                    </NavLink>
+                  </NavItem>
+                ))}
+              </Nav>
+              <div className="w-75 mx-auto">
+                {/* Passing value to the tab content */}
+                <CaseDetailsFormTabContent tabId={basicTab} fields={fields} />
+              </div>
+            </>
+          ) : (
+            <div className="text-center p-4">
+              <p>No form available for this selection.</p>
+            </div>
+          )}
         </CardBody>
       </Card>
     </Col>
--- src/Components/General/Dashboard/Organization/CaseStatus/[CaseAlias]/components/CaseDetails/components/CaseDetailsFormTabContent.tsx
+++ src/Components/General/Dashboard/Organization/CaseStatus/[CaseAlias]/components/CaseDetails/components/CaseDetailsFormTabContent.tsx
@@ -1,25 +1,20 @@
-
-import { LoanDetailsFormFields } from "@/Data/Case/CaseDetails/LoanDetails/LoanDetailsFormData";
-import { useAppSelector } from "@/Redux/Hooks";
-import { SimpleTabContentProp } from "@/Types/UiKitsType";
 import React from "react";
-import { Col, Row, TabContent, TabPane } from "reactstrap";
+import { TabContent, TabPane, Row, Col } from "reactstrap";
+import { LoanDetailsFormFields } from "@/Data/Case/CaseDetails/LoanDetails/LoanDetailsFormData";
 import FormField from "./LoanDetails/LoanDetailsFormFields";
 
-export const CaseDetailsFormTabContent: React.FC<SimpleTabContentProp> = ({
-  tabId,
-}) => {
-  const value = useAppSelector((state) => state.caseDetails.basicTabId);
+export const CaseDetailsFormTabContent: React.FC<{
+  tabId: string;
+  fields: typeof LoanDetailsFormFields;
+}> = ({ tabId, fields }) => {
+  const currentFields = fields[tabId];
 
-  const fields =
-    LoanDetailsFormFields[tabId as keyof typeof LoanDetailsFormFields];
   return (
     <TabContent activeTab={tabId} className="my-5">
       <TabPane tabId={tabId}>
         <Row className="gx-5 gy-3">
-          {fields?.map((field, idx) => (
+          {currentFields?.map((field) => (
             <Col key={field.name} md={6}>
-              {/* Each column will take up 50% of the width */}
               <FormField {...field} />
             </Col>
           ))}
