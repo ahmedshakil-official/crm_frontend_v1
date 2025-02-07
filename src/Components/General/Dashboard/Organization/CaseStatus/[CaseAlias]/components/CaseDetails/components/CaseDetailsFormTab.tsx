848cba81dd1ca37be5c8fcdb44e544d81f6e0d58
Shazzdul-Shakib <shakib1186@gmail.com>
2025-02-07 17:24:33 +0600

    next and prev button added with next page shift after save

--- src/Components/General/Dashboard/Organization/CaseStatus/[CaseAlias]/components/CaseDetails/components/CaseDetailsFormTab.tsx
+++ src/Components/General/Dashboard/Organization/CaseStatus/[CaseAlias]/components/CaseDetails/components/CaseDetailsFormTab.tsx
@@ -1,15 +1,46 @@
 import { Href } from "@/Constant";
 import { useState } from "react";
-import { Card, CardBody, Col, Nav, NavItem, NavLink } from "reactstrap";
+import { Card, CardBody, Col, Nav, NavItem, NavLink, Button } from "reactstrap";
 import { CaseDetailsFormTabContent } from "./CaseDetailsFormTabContent";
 import { CaseDetailsFormTabTitleData } from "@/Data/Case/CaseDetails/CaseDetailsFormTabTitleData";
-import { useAppSelector } from "@/Redux/Hooks";
+import { useAppDispatch, useAppSelector } from "@/Redux/Hooks";
 import { LoanDetailsFormFields } from "@/Data/Case/CaseDetails/LoanDetails/LoanDetailsFormData";
+import { basicTabIndicator } from "@/Redux/Reducers/CaseDetails/CaseDetailsTabIndicatorSlice";
 
 export const CaseDetailsFormTab = () => {
   const [basicTab, setBasicTab] = useState("1");
   const value = useAppSelector((state) => state.caseDetails.basicTabId);
+  const dispatch = useAppDispatch();
   const fields = LoanDetailsFormFields;
+
+  // Function to go to the next tab
+  const nextTab = () => {
+    const tabIds = Object.keys(fields);
+    const currentIndex = tabIds.indexOf(basicTab);
+    if (currentIndex < tabIds.length - 1) {
+      setBasicTab(tabIds[currentIndex + 1]);
+    }
+  };
+
+  // Function to go to the previous tab
+  const prevTab = () => {
+    const tabIds = Object.keys(fields);
+    const currentIndex = tabIds.indexOf(basicTab);
+    if (currentIndex > 0) {
+      setBasicTab(tabIds[currentIndex - 1]);
+    }
+  };
+
+  // Function to handle the "Save" action
+  const handleSave = () => {
+    // Replace this with your save logic
+    console.log("Form saved!");
+    dispatch(basicTabIndicator((parseInt(value) + 1).toString()));
+  };
+
+  // Check if current tab is the last one
+  const isLastTab =
+    Object.keys(fields).indexOf(basicTab) === Object.keys(fields).length - 1;
 
   return (
     <Col xxl="12">
@@ -39,8 +70,24 @@ export const CaseDetailsFormTab = () => {
                 ))}
               </Nav>
               <div className="w-75 mx-auto">
-                {/* Passing value to the tab content */}
+                {/* Pass the fields for the current active tab */}
                 <CaseDetailsFormTabContent tabId={basicTab} fields={fields} />
+              </div>
+
+              {/* Buttons to navigate between tabs */}
+              <div className="d-flex justify-content-between mt-4 w-75 mx-auto">
+                <Button
+                  onClick={prevTab}
+                  disabled={Object.keys(fields).indexOf(basicTab) === 0}
+                >
+                  Previous
+                </Button>
+                <Button
+                  onClick={isLastTab ? handleSave : nextTab}
+                  disabled={isLastTab && false} // Disable if "Save" should not be disabled
+                >
+                  {isLastTab ? "Save" : "Next"}
+                </Button>
               </div>
             </>
           ) : (
