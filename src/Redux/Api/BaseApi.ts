a4ed3bf06da7e3cd1caa89b2c5ea300a0320075a
Shazzdul-Shakib <shakib1186@gmail.com>
2025-02-11 12:05:11 +0600

    get token from sessiom

--- src/Redux/Api/BaseApi.ts
+++ src/Redux/Api/BaseApi.ts
@@ -1,11 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { getSession } from "next-auth/react";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://80.65.208.86:5000",
-    // credentials: "include",
-  prepareHeaders: (headers) => {
-    const token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQ5NTI5ODkyLCJpYXQiOjE3MzkxNjE4OTIsImp0aSI6IjJkNDEzNDUzMTJlYjQ1NzM4NDg3NzgwMjc5NTg0NzIwIiwidXNlcl9pZCI6MX0.xhZG6-niPF-rUjfnoM1iFinyJ5TxZZ0baIgLOukLUIU";
+  // credentials: "include",
+  prepareHeaders: async (headers) => {
+    const session = await getSession();
+    const token = session?.user?.accessToken;
 
     if (token) {
       headers.set("authorization", `JWT ${token}`);
