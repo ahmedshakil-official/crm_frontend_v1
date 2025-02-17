f411e2bc8db78eca9c469b93bf51a8bca7bae7b6
Shazzdul-Shakib <shakib1186@gmail.com>
2025-02-17 17:14:43 +0600

    delete dynamic approach

--- src/Components/General/Dashboard/Organization/CaseStatus/[CaseAlias]/components/CaseDetails/components/CaseDetailsTabContent.tsx
+++ src/Components/General/Dashboard/Organization/CaseStatus/[CaseAlias]/components/CaseDetails/components/CaseDetailsTabContent.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { TabContent, TabPane } from "reactstrap";
-import { CaseDetailsFormTab } from "./CaseDetailsFormTab";
 
 export const CaseDetailsTabContent: React.FC = () => {
   return (
     <TabContent>
-      <CaseDetailsFormTab/>
+      {/* <CaseDetailsFormTab/> */}
     </TabContent>
   );
 };
