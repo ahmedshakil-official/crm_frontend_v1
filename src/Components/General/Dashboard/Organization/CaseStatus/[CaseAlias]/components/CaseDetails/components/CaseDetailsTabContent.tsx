65f3740932f306b70b437fbcafa0a74360120213
Shazzdul-Shakib <shakib1186@gmail.com>
2025-02-19 10:43:20 +0600

    seperate tab component

--- src/Components/General/Dashboard/Organization/CaseStatus/[CaseAlias]/components/CaseDetails/components/CaseDetailsTabContent.tsx
+++ src/Components/General/Dashboard/Organization/CaseStatus/[CaseAlias]/components/CaseDetails/components/CaseDetailsTabContent.tsx
@@ -1,10 +1,28 @@
 import { TabContent } from "reactstrap";
 import { LoanDetailsTab } from "./LoanDetails/LoanDetailsTab";
+import { useAppSelector } from "@/Redux/Hooks";
+
+// Define a mapping of tab numbers to components
+const tabComponents: Record<number, React.FC> = {
+  1: LoanDetailsTab,
+  // 2: AnotherTab,
+};
 
 export const CaseDetailsTabContent: React.FC = () => {
+  const basicTab: string = useAppSelector(
+    (state) => state.caseDetails.basicTabId
+  );
+
+  // Convert string to number
+  const ActiveTabComponent = tabComponents[Number(basicTab)];
+
   return (
     <TabContent>
-      <LoanDetailsTab />
+      {ActiveTabComponent ? (
+        <ActiveTabComponent />
+      ) : (
+        <div className=" text-center text-warning ">No Content Available</div>
+      )}
     </TabContent>
   );
 };
