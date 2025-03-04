a7049d213cf368e69e162f5135a2fbabb18fbc96
Shazzdul-Shakib <shakib1186@gmail.com>
2025-03-04 15:55:32 +0600

    initial setup for adverse section

--- src/Components/General/Dashboard/Organization/CaseStatus/[CaseAlias]/components/CaseDetails/components/CaseDetailsTabContent.tsx
+++ src/Components/General/Dashboard/Organization/CaseStatus/[CaseAlias]/components/CaseDetails/components/CaseDetailsTabContent.tsx
@@ -3,12 +3,14 @@ import { TabContent } from "reactstrap";
 import { ApplicantsDetailsTab } from "./ApplicantsDetails/ApplicantsDetailsTab";
 import { EmploymentTab } from "./Employment/EmploymentTab";
 import { LoanDetailsTab } from "./LoanDetails/LoanDetailsTab";
+import { AdverseTab } from "./Adverse/AdverseTab";
 
 // Define a mapping of tab names to components
 const tabComponents: Record<string, React.FC> = {
   "Loan Details": LoanDetailsTab,
   "Applicant(s) Details": ApplicantsDetailsTab,
   "Employment/Income": EmploymentTab,
+  "Adverse": AdverseTab,
 };
 
 export const CaseDetailsTabContent: React.FC = () => {
