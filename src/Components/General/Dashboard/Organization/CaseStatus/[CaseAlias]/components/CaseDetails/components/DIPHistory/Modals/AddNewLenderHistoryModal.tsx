12a925fc88f7d2a5d243ad76270a63c24ca92546
Md Mahbub Rahman <54384173+mahbub045@users.noreply.github.com>
2025-04-11 11:05:38 +0600

    Rearrange single caseDetails dipHistory

--- src/Components/General/Dashboard/Organization/CaseStatus/[CaseAlias]/components/CaseDetails/components/DIPHistory/Modals/AddNewLenderHistoryModal.tsx
+++ src/Components/General/Dashboard/Organization/CaseStatus/[CaseAlias]/components/CaseDetails/components/DIPHistory/Modals/AddNewLenderHistoryModal.tsx
@@ -1,4 +1,4 @@
-import { useAddDIPHistoryDetailsMutation } from "@/Redux/Reducers/CaseDetails/DIPHistoryDetails/DIPHistoryDetailsApi";
+import { useAddDIPHistoryDetailsMutation } from "@/Redux/Reducers/Cases/SingleCaseInfo/CaseDetails/DIPHistoryDetails/DIPHistoryDetailsApi";
 import LoadingSpinner from "@/app/loading";
 import { useParams } from "next/navigation";
 import React, { useState } from "react";
