4adaee520cdf740959de68d771bc6d78e2634379
Shazzdul-Shakib <shakib1186@gmail.com>
2025-04-17 16:47:15 +0600

    resuable card created for tab contents

--- src/Components/General/Dashboard/Organization/CaseStatus/[CaseAlias]/components/CaseDetails/components/Compliance/ComplianceTabContents/AntiMoneyLaunderingTabContent.tsx
+++ src/Components/General/Dashboard/Organization/CaseStatus/[CaseAlias]/components/CaseDetails/components/Compliance/ComplianceTabContents/AntiMoneyLaunderingTabContent.tsx
@@ -1,6 +1,43 @@
 import { FC } from "react";
+import { DisclosureItem } from "../CompianceTabContents/components/DisclosureItem";
+
+const amlData = [
+  {
+    reference: "3.1",
+    title: "Nivo IDV check (Run & Completed)",
+    answer: null,
+  },
+  {
+    reference: "3.2",
+    title: "Financial Sanctions checked and copy on file (dated prior to research)",
+    answer: null,
+  },
+  {
+    reference: "3.3",
+    title: "Proof of ID (In Date & Certified)",
+    answer: null,
+  },
+  {
+    reference: "3.4",
+    title: "Proof of Address (In Date & Certified)",
+    answer: null,
+  }
+];
+
 const AntiMoneyLaunderingTabContent: FC = () => {
-  return <div>Anti-Money Laundering</div>;
+  return (
+    <div className="p-3">
+      {amlData.map((item, index) => (
+        <DisclosureItem
+          key={item.reference}
+          reference={item.reference}
+          title={item.title}
+          answer={item.answer}
+          index={index}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default AntiMoneyLaunderingTabContent;
--- src/Components/General/Dashboard/Organization/CaseStatus/[CaseAlias]/components/CaseDetails/components/Compliance/ComplianceTabContents/DataProtectionTabContent.tsx
+++ src/Components/General/Dashboard/Organization/CaseStatus/[CaseAlias]/components/CaseDetails/components/Compliance/ComplianceTabContents/DataProtectionTabContent.tsx
@@ -1,6 +1,29 @@
 import { FC } from "react";
+import { DisclosureItem } from "../CompianceTabContents/components/DisclosureItem";
+
+const dataProtectionData = [
+  {
+    reference: "2.1",
+    title: "Factfind Filled",
+    answer: null,
+  },
+
+];
+
 const DataProtectionTabContent: FC = () => {
-  return <div>Data Protection - GDPR</div>;
+  return (
+    <div className="p-3">
+      {dataProtectionData.map((item, index) => (
+        <DisclosureItem
+          key={item.reference}
+          reference={item.reference}
+          title={item.title}
+          answer={item.answer}
+          index={index}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default DataProtectionTabContent;
