6372234f49c56ee71c598780d9dc6368bebe7bed
Md Mahbub Rahman <54384173+mahbub045@users.noreply.github.com>
2025-04-18 16:23:13 +0600

    Remove Social icon

--- src/Components/General/Dashboard/Network/Organizations/[OrganizationSlug]/OrganizationProfile/OrganizationBanner.tsx
+++ src/Components/General/Dashboard/Network/Organizations/[OrganizationSlug]/OrganizationProfile/OrganizationBanner.tsx
@@ -116,23 +116,13 @@ const OrganizationBanner: React.FC<FetchSingleOrganizationProps> = ({
                 </Button>
               </Link>
               <Link href="">
-                <Button color="danger" className="mx-2">
-                  <i className="fa-regular fa-envelope"></i>
-                </Button>
-              </Link>
-              <Link href="">
-                <Button color="info" className="mx-2">
+                <Button color="dark" className="mx-2">
                   <i className="fa-brands fa-twitter"></i>
                 </Button>
               </Link>
               <Link href={`${organizationInfo?.website}`} target="_blank">
                 <Button color="success" className="mx-2">
                   <i className="fa-solid fa-earth-americas"></i>
-                </Button>
-              </Link>
-              <Link href="">
-                <Button color="dark" className="mx-2">
-                  <i className="fa-brands fa-instagram"></i>
                 </Button>
               </Link>
             </div>
