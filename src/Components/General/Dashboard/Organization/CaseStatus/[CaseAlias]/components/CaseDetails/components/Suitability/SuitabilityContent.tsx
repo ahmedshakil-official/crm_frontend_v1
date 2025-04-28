36b5067cfb98efd4d2c544f55d2a80ae8ceb49e2
Md Mahbub Rahman <54384173+mahbub045@users.noreply.github.com>
2025-04-28 14:20:07 +0600

    Complete budget_affordability

--- src/Components/General/Dashboard/Organization/CaseStatus/[CaseAlias]/components/CaseDetails/components/Suitability/SuitabilityContent.tsx
+++ src/Components/General/Dashboard/Organization/CaseStatus/[CaseAlias]/components/CaseDetails/components/Suitability/SuitabilityContent.tsx
@@ -26,10 +26,17 @@ const SuitabilityContent: React.FC = () => {
     circumstances_objectives: {
       circumstances_type: "",
       question_one_answer: "",
-      question_one_sharia: "",
       question_two_answer: "",
-      question_two_sharia: "",
       question_three_answer: "",
+      question_one_sharia: "",
+      question_two_sharia: "",
+    },
+    budget_affordability: {
+      budget_affordability_type: "",
+      question_one_answer: "",
+      question_two_answer: "",
+      question_one_sharia: "",
+      question_two_sharia: "",
     },
   });
 
@@ -37,13 +44,13 @@ const SuitabilityContent: React.FC = () => {
   const { data: suitabilityData, isLoading } = useGetSuitabilityQuery({
     case_alias: casealias,
   });
-  console.log("SD:", suitabilityData);
+
   const [updateSuitability, { isLoading: isUpdating }] =
     useUpdateSuitabilityMutation();
 
   // Set initial form data when API data is available
   useEffect(() => {
-    if (suitabilityData?.circumstances_objectives) {
+    if (suitabilityData?.circumstances_objectives || suitabilityData?.budget_affordability) {
       setFormValue({
         ...formValue,
         circumstances_objectives: {
@@ -66,19 +73,37 @@ const SuitabilityContent: React.FC = () => {
             suitabilityData.circumstances_objectives.question_two_sharia ||
             defaultAnswers.circumstancesAndObjectives_S_A2,
         },
+        budget_affordability: {
+          budget_affordability_type:
+            suitabilityData.budget_affordability.budget_affordability_type ||
+            "GENERAL",
+          question_one_answer:
+            suitabilityData.budget_affordability.question_one_answer ||
+            defaultAnswers.budgetAndAffordability_G_A1,
+          question_two_answer:
+            suitabilityData.budget_affordability.question_two_answer ||
+            defaultAnswers.budgetAndAffordability_G_A2,
+          question_one_sharia:
+            suitabilityData.budget_affordability.question_one_sharia ||
+            defaultAnswers.budgetAndAffordability_S_A1,
+          question_two_sharia:
+            suitabilityData.budget_affordability.question_two_sharia ||
+            defaultAnswers.budgetAndAffordability_S_A2,
+        },
       });
     }
   }, [suitabilityData]);
 
   // Handle input changes for nested state
-  const handleChange = (
-    field: keyof (typeof formValue)["circumstances_objectives"],
+  const handleChange = <T extends keyof typeof formValue>(
+    section: T,
+    field: keyof (typeof formValue)[T],
     value: string
   ) => {
     setFormValue((prev) => ({
       ...prev,
-      circumstances_objectives: {
-        ...prev.circumstances_objectives,
+      [section]: {
+        ...prev[section],
         [field]: value,
       },
     }));
@@ -87,19 +112,19 @@ const SuitabilityContent: React.FC = () => {
   // Handle form submission
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-
     const payload = {
       alias: casealias,
       circumstances_objectives: {
         ...formValue.circumstances_objectives,
       },
+      budget_affordability: {
+        ...formValue.budget_affordability,
+      },
     };
-
     try {
       await updateSuitability({
         payload: payload,
         case_alias: casealias,
-        // suitability_alias: suitabilityData.alias,
       }).unwrap();
       toast.success("Changes saved successfully!");
     } catch (error) {
@@ -131,7 +156,11 @@ const SuitabilityContent: React.FC = () => {
                 name="circumstances_type"
                 value={formValue.circumstances_objectives.circumstances_type}
                 onChange={(e) =>
-                  handleChange("circumstances_type", e.target.value)
+                  handleChange(
+                    "circumstances_objectives",
+                    "circumstances_type",
+                    e.target.value
+                  )
                 }
               >
                 <option value="GENERAL">General</option>
@@ -152,7 +181,11 @@ const SuitabilityContent: React.FC = () => {
                   rows="3"
                   value={formValue.circumstances_objectives.question_one_answer}
                   onChange={(e) =>
-                    handleChange("question_one_answer", e.target.value)
+                    handleChange(
+                      "circumstances_objectives",
+                      "question_one_answer",
+                      e.target.value
+                    )
                   }
                 />
               </FormGroup>
@@ -164,7 +197,11 @@ const SuitabilityContent: React.FC = () => {
                   rows="3"
                   value={formValue.circumstances_objectives.question_two_answer}
                   onChange={(e) =>
-                    handleChange("question_two_answer", e.target.value)
+                    handleChange(
+                      "circumstances_objectives",
+                      "question_two_answer",
+                      e.target.value
+                    )
                   }
                 />
               </FormGroup>
@@ -178,7 +215,11 @@ const SuitabilityContent: React.FC = () => {
                     formValue.circumstances_objectives.question_three_answer
                   }
                   onChange={(e) =>
-                    handleChange("question_three_answer", e.target.value)
+                    handleChange(
+                      "circumstances_objectives",
+                      "question_three_answer",
+                      e.target.value
+                    )
                   }
                 />
               </FormGroup>
@@ -195,7 +236,11 @@ const SuitabilityContent: React.FC = () => {
                   rows="6"
                   value={formValue.circumstances_objectives.question_one_sharia}
                   onChange={(e) =>
-                    handleChange("question_one_sharia", e.target.value)
+                    handleChange(
+                      "circumstances_objectives",
+                      "question_one_sharia",
+                      e.target.value
+                    )
                   }
                 />
               </FormGroup>
@@ -207,7 +252,11 @@ const SuitabilityContent: React.FC = () => {
                   rows="3"
                   value={formValue.circumstances_objectives.question_two_sharia}
                   onChange={(e) =>
-                    handleChange("question_two_sharia", e.target.value)
+                    handleChange(
+                      "circumstances_objectives",
+                      "question_two_sharia",
+                      e.target.value
+                    )
                   }
                 />
               </FormGroup>
@@ -220,7 +269,113 @@ const SuitabilityContent: React.FC = () => {
       </Card>
 
       {/* Budget and affordability */}
-      {/* Add additional fields here if needed */}
+      <Card className="border-1 border-success mt-3">
+        <CardHeader className="d-flex justify-content-between align-items-center">
+          <Col md={6}>
+            <h4>Budget and affordability</h4>
+          </Col>
+          <Col md={4}>
+            <FormGroup>
+              <Input
+                type="select"
+                name="budget_affordability_type"
+                value={
+                  formValue.budget_affordability.budget_affordability_type
+                }
+                onChange={(e) =>
+                  handleChange(
+                    "budget_affordability",
+                    "budget_affordability_type",
+                    e.target.value
+                  )
+                }
+              >
+                <option value="GENERAL">General</option>
+                <option value="SHARIA">Sharia</option>
+              </Input>
+            </FormGroup>
+          </Col>
+        </CardHeader>
+        <CardBody>
+          {formValue.budget_affordability.budget_affordability_type ===
+            "GENERAL" && (
+            <>
+              <FormGroup>
+                <Label>Answer 1</Label>
+                <Input
+                  type="textarea"
+                  name="question_one_answer"
+                  rows="3"
+                  value={formValue.budget_affordability.question_one_answer}
+                  onChange={(e) =>
+                    handleChange(
+                      "budget_affordability",
+                      "question_one_answer",
+                      e.target.value
+                    )
+                  }
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label>Answer 2</Label>
+                <Input
+                  type="textarea"
+                  name="question_two_answer"
+                  rows="3"
+                  value={formValue.budget_affordability.question_two_answer}
+                  onChange={(e) =>
+                    handleChange(
+                      "budget_affordability",
+                      "question_two_answer",
+                      e.target.value
+                    )
+                  }
+                />
+              </FormGroup>
+            </>
+          )}
+          {formValue.budget_affordability.budget_affordability_type ===
+            "SHARIA" && (
+            <>
+              <FormGroup>
+                <Label>Answer 1</Label>
+                <Input
+                  type="textarea"
+                  name="question_one_sharia"
+                  rows="6"
+                  value={formValue.budget_affordability.question_one_sharia}
+                  onChange={(e) =>
+                    handleChange(
+                      "budget_affordability",
+                      "question_one_sharia",
+                      e.target.value
+                    )
+                  }
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label>Answer 2</Label>
+                <Input
+                  type="textarea"
+                  name="question_two_sharia"
+                  rows="3"
+                  value={formValue.budget_affordability.question_two_sharia}
+                  onChange={(e) =>
+                    handleChange(
+                      "budget_affordability",
+                      "question_two_sharia",
+                      e.target.value
+                    )
+                  }
+                />
+              </FormGroup>
+            </>
+          )}
+          <div className="d-flex justify-content-start align-items-center">
+            <Button color="success">Add More Answer</Button>
+          </div>
+        </CardBody>
+      </Card>
 
       {/* Button for save changes */}
       <div className="d-flex justify-content-end mt-3 mb-0">
