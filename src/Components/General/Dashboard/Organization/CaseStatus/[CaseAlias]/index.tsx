61b1e699caeefd6926aab1c2a4dc3807db82fbb6
mahbub045 <mahbubrmn45@gmail.com>
2025-05-15 12:26:49 +0600

    Wrong alias, slug error handle

--- src/Components/General/Dashboard/Organization/CaseStatus/[CaseAlias]/index.tsx
+++ src/Components/General/Dashboard/Organization/CaseStatus/[CaseAlias]/index.tsx
@@ -2,7 +2,7 @@ import { Dashboard, Organization, OrganizationTitle } from "@/Constant";
 import { useGetSingleCaseDetailsQuery } from "@/Redux/Reducers/Organization/Cases/CaseDetailsApi";
 import { useGetJointUserInfoQuery } from "@/Redux/Reducers/Organization/Cases/SingleCaseInfo/JointUser/JointUserDetailsApi";
 import { CaseInfo } from "@/Types/Organization/Cases/CaseTypes";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { Container, Row } from "reactstrap";
 import CalenderContainer from "./components/Calender/CalenderContainer";
@@ -12,24 +12,55 @@ import JointUsers from "./components/JointUsers/JointUsers";
 import MeetingHistory from "./components/MeetingHistory/MeetingHistory";
 import SingleCaseBreadcrumbs from "./components/SingleCaseBreadcrumbs";
 import SingleCaseInfo from "./components/SingleCaseInfo/SingleCaseInfo";
+import LoadingSpinner from "@/app/loading";
+import { toast } from "react-toastify";
 
 const CaseContainer: React.FC = () => {
   const [caseInfo, setCaseInfo] = useState<CaseInfo>();
   const params = useParams();
   const { casealias } = params;
+  const router = useRouter();
 
   // rtk hooks
-  const { data: jointUserInfo, isLoading: isJointUserFetcing } =
-    useGetJointUserInfoQuery({
-      case_alias: casealias,
-    });
+  const { data: jointUserInfo, isLoading: isJointUserFetcing } = useGetJointUserInfoQuery(
+    { case_alias: casealias },
+    { skip: !casealias }
+  );
 
-  const { data: caseData, isLoading } = useGetSingleCaseDetailsQuery({
-    case_alias: casealias,
-  });
+  const { data: caseData, isLoading, isError } = useGetSingleCaseDetailsQuery(
+    { case_alias: casealias },
+    { skip: !casealias }
+  );
+
   useEffect(() => {
-    setCaseInfo(caseData || {});
-  }, [caseData]);
+    if (!isLoading) {
+      if (isError || !caseData) {
+        router.push("/dashboard/organization");
+        toast.error("Find Wrong URL! Redirecting...");
+        return;
+      }
+
+      if (caseData.alias !== casealias) {
+        router.push("/dashboard/organization");
+        toast.error("Find Wrong URL! Redirecting...");
+        return;
+      }
+
+      setCaseInfo(caseData);
+    }
+  }, [caseData, casealias, router, isLoading, isError]);
+
+  if (isLoading) {
+    return (
+      <div className="p-4">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (isError || !caseInfo) {
+    return null; // Will redirect in useEffect
+  }
 
   return (
     <>
