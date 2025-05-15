92a17c377e943ec0f6029491a2814d7c6090646e
mahbub045 <mahbubrmn45@gmail.com>
2025-05-15 15:49:42 +0600

    Fix Commom error page dashboard redirect role base

--- src/Components/General/Dashboard/Client/Components/MyApplications.tsx
+++ src/Components/General/Dashboard/Client/Components/MyApplications.tsx
@@ -23,62 +23,62 @@ const MyApplications: React.FC = () => {
         <h5 className="mb-0">My Applications</h5>
       </CardHeader>
       <CardBody className="p-0">
-        <div className="table-responsive">
-          <Table hover borderedd className="mb-0 text-center">
-            <thead>
-              <tr>
-                <th>Case #</th>
-                <th>Create Date</th>
-                <th>Case Category</th>
-                <th>Stage</th>
-                <th>Lead</th>
-                <th>Phone Number</th>
-                <th>Action</th>
+        <Table responsive hover borderedd className="mb-0 text-center">
+          <thead>
+            <tr>
+              <th>Case #</th>
+              <th>Create Date</th>
+              <th>Case Category</th>
+              <th>Stage</th>
+              <th>Lead</th>
+              <th>Phone Number</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {applications?.map((app: SingleClientApplicationProps) => (
+              <tr key={app.name}>
+                <td>
+                  <Link
+                    className="text_decoration_hover"
+                    href={`client/${app.alias}`}
+                  >
+                    {app.name}
+                  </Link>
+                </td>
+                <td>{new Date(app.created_at).toLocaleDateString("en-GB")}</td>
+                <td>
+                  {app.case_category?.charAt(0).toUpperCase() +
+                    app.case_category?.slice(1).toLocaleLowerCase()}
+                </td>
+                <td>
+                  {app.case_stage
+                    ?.split("_")
+                    .map(
+                      (word) =>
+                        word.charAt(0).toUpperCase() +
+                        word.slice(1).toLowerCase()
+                    )
+                    .join(" ")}
+                </td>
+                <td>
+                  {app.lead_user.first_name + " " + app.lead_user.last_name}
+                </td>
+                <td>{app.lead_user.phone || "-"}</td>
+                <td>
+                  <Link href={`client/${app.alias}`}>
+                    <button className="btn btn-primary btn-sm">Continue</button>
+                  </Link>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {applications?.map((app: SingleClientApplicationProps) => (
-                <tr key={app.name}>
-                  <td>
-                    <Link
-                      className="text_decoration_hover"
-                      href={`client/${app.alias}`}
-                    >
-                      {app.name}
-                    </Link>
-                  </td>
-                  <td>
-                    {new Date(app.created_at).toLocaleDateString("en-GB")}
-                  </td>
-                  <td>
-                    {app.case_category?.charAt(0).toUpperCase() +
-                      app.case_category?.slice(1).toLocaleLowerCase()}
-                  </td>
-                  <td>
-                    {app.case_stage
-                      ?.split("_")
-                      .map(
-                        (word) =>
-                          word.charAt(0).toUpperCase() +
-                          word.slice(1).toLowerCase()
-                      )
-                      .join(" ")}
-                  </td>
-                  <td>
-                    {app.lead_user.first_name + " " + app.lead_user.last_name}
-                  </td>
-                  <td>{app.lead_user.phone || "-"}</td>
-                  <td>
-                    <Link href={`client/${app.alias}`}>
-                      <button className="btn btn-primary btn-sm">
-                        Continue
-                      </button>
-                    </Link>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
+            ))}
+          </tbody>
+        </Table>
+        <div className="px-2 pb-2">
+          <p className="text-success">
+            Showing 1 to {applications?.length || 0} of{" "}
+            {applications?.length || 0} cases
+          </p>
         </div>
       </CardBody>
     </Card>
