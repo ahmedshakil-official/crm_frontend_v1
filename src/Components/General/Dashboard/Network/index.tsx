2ce16d2e04bbcfefbf7535bb75e04c486402e98b
mahbub045 <mahbubrmn45@gmail.com>
2025-05-22 10:55:35 +0600

    Edit Breadcrumbs

--- src/Components/General/Dashboard/Network/index.tsx
+++ src/Components/General/Dashboard/Network/index.tsx
@@ -1,9 +1,4 @@
 import { Container, Row } from "reactstrap";
-import CaseStatus from "../Organization/CaseStatus/CaseStatus";
-import AdvisorList from "../Organization/Directors/AdvisorList/AdvisorList";
-import ClientList from "../Organization/Directors/ClientList/ClientList";
-import IntroducerList from "../Organization/Directors/IntroducerList/IntroducerList";
-import LeadList from "../Organization/Directors/LeadList/LeadList";
 import NetworkBreadcrumbs from "./NetworkBreadcrumbs/Breadcrumbs";
 import OrganizationCards from "./Organizations/OrganizationCards/OrganizationCards";
 
@@ -11,27 +6,15 @@ const ContainerNetwork = () => {
   return (
     <>
       <NetworkBreadcrumbs />
-      <Container fluid className="default-dashboard">
+      <Container fluid>
+        <Row></Row>
+        <Row></Row>
+        <Row></Row>
+        <Row></Row>
+        <Row></Row>
         <Row>
           <OrganizationCards />
         </Row>
-        {/* load Organization dashboard data  */}
-        <Row>
-          <CaseStatus />
-        </Row>
-        <Row>
-          <LeadList />
-        </Row>
-        <Row>
-          <ClientList />
-        </Row>
-        <Row>
-          <AdvisorList />
-        </Row>
-        <Row>
-          <IntroducerList />
-        </Row>
-        {/* load Organization dashboard data end */}
       </Container>
     </>
   );
