d88bf7dc4099e79adb6ed51c29df6c14b6d24ef5
mahbub045 <mahbubrmn45@gmail.com>
2025-06-01 15:59:49 +0600

    Add Breadcrumbs

--- src/Components/General/Dashboard/Network/UserSettings/SecurityPolicy/index.tsx
+++ src/Components/General/Dashboard/Network/UserSettings/SecurityPolicy/index.tsx
@@ -1,9 +1,18 @@
+import { Col, Container, Row } from "reactstrap";
+import Breadcrumbs from "./Breadcrumbs/Breadcrumbs";
+
 const SecurityPolicyContainer: React.FC = () => {
   return (
-    <div>
-      {/* JSX here */}
-      <h1>Security Policy</h1>
-    </div>
+    <>
+      <Breadcrumbs />
+      <Container fluid>
+        <Row>
+          <Col>
+            <h1>Security Policy</h1>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
