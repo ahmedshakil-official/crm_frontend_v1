caa3a761d28ec0d35ab1d6eae9d4545b65ce953b
mahbub045 <mahbubrmn45@gmail.com>
2025-06-16 16:24:23 +0600

    Fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,17 @@
 
 import NavBar from "@/Components/Home/Components/NavBar/NavBar";
 import UnderDevelopment from "@/Components/Other/UnderDevelopment/UnderDevelopment";
+import { Container } from "reactstrap";
 
 export default function Home() {
   return (
-    <main>
-      <NavBar />
-      <UnderDevelopment />
-    </main>
+    <>
+      <main>
+        <NavBar />
+      </main>
+      <Container fluid>
+        <UnderDevelopment />
+      </Container>
+    </>
   );
 }
