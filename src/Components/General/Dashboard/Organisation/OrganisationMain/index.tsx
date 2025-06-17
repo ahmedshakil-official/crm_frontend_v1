d9bba4b279ac9f08d612c373df7a95036d20bc45
mahbub045 <mahbubrmn45@gmail.com>
2025-06-17 17:15:04 +0600

    Client Growth chart done

--- src/Components/General/Dashboard/Organisation/OrganisationMain/index.tsx
+++ src/Components/General/Dashboard/Organisation/OrganisationMain/index.tsx
@@ -3,6 +3,7 @@ import OrganisationBreadcrumbs from "../Breadcrumbs/Breadcrumbs";
 import CaseCompletionOverTime from "./CaseCompletionOverTime/CaseCompletionOverTime";
 import DashboardOverview from "./DashboardOverview/DashboardOverview";
 import MonthlyRevenueTrend from "./MonthlyRevenueTrend/MonthlyRevenueTrend";
+import ClientGrowth from "./ClientGrowth/ClientGrowth";
 
 const OrganisationContainer = () => {
   return (
@@ -22,6 +23,17 @@ const OrganisationContainer = () => {
           </Col>
           <Col md={6} sm={12}>
             <CaseCompletionOverTime />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={4} sm={12}>
+            <ClientGrowth />
+          </Col>
+          <Col md={4} sm={12}>
+            
+          </Col>
+          <Col md={4} sm={12}>
+           
           </Col>
         </Row>
       </Container>
