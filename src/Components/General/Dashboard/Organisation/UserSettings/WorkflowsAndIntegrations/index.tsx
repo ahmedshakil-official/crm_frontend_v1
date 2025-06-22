7d9d9ed0d2247d27ea1f9400d1880f73993c0e68
mahbub045 <mahbubrmn45@gmail.com>
2025-06-22 11:40:41 +0600

    WorkflowsAndIntegrationsOverview design done

--- src/Components/General/Dashboard/Organisation/UserSettings/WorkflowsAndIntegrations/index.tsx
+++ src/Components/General/Dashboard/Organisation/UserSettings/WorkflowsAndIntegrations/index.tsx
@@ -1,8 +1,20 @@
+import { Container } from "reactstrap";
+import OrganisationBreadcrumbs from "../../Breadcrumbs/Breadcrumbs";
+import WorkflowsAndIntegrationsOverview from "./WorkflowsAndIntegrationsOverview/WorkflowsAndIntegrationsOverview";
+
 const WorkflowsAndIntegrationContainer: React.FC = () => {
   return (
-    <div>
-      {/* JSX here */}WorkflowsAndIntegrationContainer
-    </div>
+    <>
+      <OrganisationBreadcrumbs
+        mainTitle="Workflows & Integrations"
+        title="Manage workflows and integrations"
+        parent="User Settings"
+        activePage="Workflows & Integrations"
+      />
+      <Container fluid>
+        <WorkflowsAndIntegrationsOverview />
+      </Container>
+    </>
   );
 };
 
