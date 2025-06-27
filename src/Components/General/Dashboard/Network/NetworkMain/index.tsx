c13118b479a67d424ccd63c1ad31bfdc1ca79574
mahbub045 <mahbubrmn45@gmail.com>
2025-06-27 16:51:50 +0600

    fix

--- src/Components/General/Dashboard/Network/NetworkMain/index.tsx
+++ src/Components/General/Dashboard/Network/NetworkMain/index.tsx
@@ -2,12 +2,11 @@ import { Container, Row } from "reactstrap";
 import NetworkBreadcrumbs from "../Breadcrumbs/Breadcrumbs";
 import Charts from "./Charts/Charts";
 import NetworkPerformance from "./NetworkPerformance/NetworkPerformance";
-import Organizations from "./Organizations/Organizations";
+import Organisations from "./Organisations/Organisations";
 import PerformanceOverview from "./PerformanceOverview/PerformanceOverview";
 import ProductPenetration from "./ProductPenetration/ProductPenetration";
 import RecentActivity from "./RecentActivity/RecentActivity";
 import TopPerformingBrokers from "./TopPerformingBrokers/TopPerformingBrokers";
-import Organisations from "./Organisations/Organisations";
 
 const ContainerNetwork = () => {
   return (
