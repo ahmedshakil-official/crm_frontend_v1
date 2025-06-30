f52df876e5d70eb89ca1e7912030af72b00e0f77
mahbub045 <mahbubrmn45@gmail.com>
2025-06-30 15:25:02 +0600

    Marketing hub design done

--- src/Components/General/Dashboard/OrganisationAdviser/MarketingHub/index.tsx
+++ src/Components/General/Dashboard/OrganisationAdviser/MarketingHub/index.tsx
@@ -1,16 +1,17 @@
 import { Container } from "reactstrap";
 import OrganisationAdviserBreadcrumbs from "../Breadcrumbs/Breadcrumbs";
+import MarketingHubOverview from "./MarketingHubOverview/MarketingHubOverview";
 
 const OrganisationAdviserMarketingHubContainer: React.FC = () => {
   return (
     <>
       <OrganisationAdviserBreadcrumbs
         mainTitle="Marketing Hub"
-        title="Hello there!"
+        title="Manage your social media and WhatsApp campaigns"
         activePage="Marketing Hub"
       />
       <Container fluid>
-        <h1 className="text-danger text-center">Under Development</h1>
+        <MarketingHubOverview />
       </Container>
     </>
   );
