ec6d1b6907378cb176ff165a2eaaa8d5cad04d56
mahbub045 <mahbubrmn45@gmail.com>
2025-07-02 12:54:08 +0600

    Whatsapp tab complete

--- src/Components/General/Dashboard/OrganisationAdviser/MarketingHub/QuickGlance/WhatsAppTab/WhatsAppTab.tsx
+++ src/Components/General/Dashboard/OrganisationAdviser/MarketingHub/QuickGlance/WhatsAppTab/WhatsAppTab.tsx
@@ -2,6 +2,7 @@ import { Col, Row } from "reactstrap";
 import CampaignHistory from "./CampaignHistory/CampaignHistory";
 import ComposeWhatsAppMessage from "./ComposeWhatsAppMessage/ComposeWhatsAppMessage";
 import MessageTemplates from "./MessageTemplates/MessageTemplates";
+import WhatsAppContactList from "./WhatsAppContactList/WhatsAppContactList";
 
 const WhatsAppTab: React.FC = () => {
   return (
@@ -17,6 +18,11 @@ const WhatsAppTab: React.FC = () => {
       <Row>
         <Col md={12}>
           <CampaignHistory />
+        </Col>
+      </Row>
+      <Row>
+        <Col md={12}>
+          <WhatsAppContactList />
         </Col>
       </Row>
     </>
