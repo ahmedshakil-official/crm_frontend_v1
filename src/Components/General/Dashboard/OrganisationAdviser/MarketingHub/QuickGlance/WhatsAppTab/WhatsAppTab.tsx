87c41d1e09bdc3142ca6ff83790c32e1c3284abd
mahbub045 <mahbubrmn45@gmail.com>
2025-07-02 12:24:22 +0600

    CampaignHistory design done

--- src/Components/General/Dashboard/OrganisationAdviser/MarketingHub/QuickGlance/WhatsAppTab/WhatsAppTab.tsx
+++ src/Components/General/Dashboard/OrganisationAdviser/MarketingHub/QuickGlance/WhatsAppTab/WhatsAppTab.tsx
@@ -1,17 +1,25 @@
 import { Col, Row } from "reactstrap";
+import CampaignHistory from "./CampaignHistory/CampaignHistory";
 import ComposeWhatsAppMessage from "./ComposeWhatsAppMessage/ComposeWhatsAppMessage";
 import MessageTemplates from "./MessageTemplates/MessageTemplates";
 
 const WhatsAppTab: React.FC = () => {
   return (
-    <Row>
-      <Col md={6}>
-        <ComposeWhatsAppMessage />
-      </Col>
-      <Col md={6}>
-        <MessageTemplates />
-      </Col>
-    </Row>
+    <>
+      <Row>
+        <Col md={6}>
+          <ComposeWhatsAppMessage />
+        </Col>
+        <Col md={6}>
+          <MessageTemplates />
+        </Col>
+      </Row>
+      <Row>
+        <Col md={12}>
+          <CampaignHistory />
+        </Col>
+      </Row>
+    </>
   );
 };
 
