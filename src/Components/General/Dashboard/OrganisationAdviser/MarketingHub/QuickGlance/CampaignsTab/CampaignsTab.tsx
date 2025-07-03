9d3a0e7c5ea889d1e888f3c5662b64332d8cf728
mahbub045 <mahbubrmn45@gmail.com>
2025-07-03 12:37:35 +0600

    Active campaings  design done

--- src/Components/General/Dashboard/OrganisationAdviser/MarketingHub/QuickGlance/CampaignsTab/CampaignsTab.tsx
+++ src/Components/General/Dashboard/OrganisationAdviser/MarketingHub/QuickGlance/CampaignsTab/CampaignsTab.tsx
@@ -1,13 +1,21 @@
 import { Col, Row } from "reactstrap";
+import ActiveCampaigns from "./ActiveCampaigns/ActiveCampaigns";
 import LeadAdCampaigns from "./LeadAdCampaigns/LeadAdCampaigns";
 
 const CampaignsTab: React.FC = () => {
   return (
-    <Row>
-      <Col md="12">
-        <LeadAdCampaigns />
-      </Col>
-    </Row>
+    <>
+      <Row>
+        <Col md="12">
+          <LeadAdCampaigns />
+        </Col>
+      </Row>
+      <Row>
+        <Col md="12">
+          <ActiveCampaigns />
+        </Col>
+      </Row>
+    </>
   );
 };
 
