89db4fea7e67a04bccee3eb1882d8d467e476732
mahbub045 <mahbubrmn45@gmail.com>
2025-07-07 14:41:22 +0600

    Analytics tab WhatsApp Campaign Performance and Social Media Performance done

--- src/Components/General/Dashboard/OrganisationAdviser/MarketingHub/QuickGlance/AnalyticsTab/AnalyticsTab.tsx
+++ src/Components/General/Dashboard/OrganisationAdviser/MarketingHub/QuickGlance/AnalyticsTab/AnalyticsTab.tsx
@@ -1,5 +1,7 @@
 import { Col, Row } from "reactstrap";
 import MarketingAnalyticsOverview from "./MarketingAnalyticsOverview/MarketingAnalyticsOverview";
+import SocialMediaPerformance from "./SocialMediaPerformance/SocialMediaPerformance";
+import WhatsAppCampaignPerformance from "./WhatsAppCampaignPerformance/WhatsAppCampaignPerformance";
 
 const AnalyticsTab: React.FC = () => {
   return (
@@ -7,6 +9,14 @@ const AnalyticsTab: React.FC = () => {
       <Row>
         <Col md="12">
           <MarketingAnalyticsOverview />
+        </Col>
+      </Row>
+      <Row>
+        <Col md="6" sm="12">
+          <SocialMediaPerformance />
+        </Col>
+        <Col md="6" sm="12">
+          <WhatsAppCampaignPerformance />
         </Col>
       </Row>
     </>
---
+++ src/Components/General/Dashboard/OrganisationAdviser/MarketingHub/QuickGlance/AnalyticsTab/WhatsAppCampaignPerformance/WhatsAppCampaignPerformance.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Chart } from "react-google-charts";
+import { Card, CardBody } from "reactstrap";
+
+const data = [
+  ["Week", "Delivered", "Read", "Replied"],
+  ["Week 1", 120, 100, 10],
+  ["Week 2", 150, 110, 15],
+  ["Week 3", 90, 60, 8],
+  ["Week 4", 200, 160, 20],
+];
+
+const options = {
+  chartArea: { width: "70%" },
+  colors: ["#4285F4", "#34A853", "#FBBC05"],
+  curveType: "function",
+  legend: { position: "top" },
+  hAxis: {
+    title: "Week",
+  },
+  vAxis: {
+    title: "Count",
+    minValue: 0,
+  },
+  pointSize: 5,
+};
+
+const WhatsAppCampaignPerformance: React.FC = () => {
+  return (
+    <Card>
+      <CardBody>
+        <h2>WhatsApp Campaign Performance</h2>
+        <p>Message delivery and engagement rates</p>
+        <Chart
+          chartType="LineChart"
+          width="100%"
+          height="300px"
+          data={data}
+          options={options}
+        />
+      </CardBody>
+    </Card>
+  );
+};
+
+export default WhatsAppCampaignPerformance;
