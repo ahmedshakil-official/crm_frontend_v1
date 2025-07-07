cc5062e18af988a62cf3c36094e6da988738be9b
mahbub045 <mahbubrmn45@gmail.com>
2025-07-07 14:59:45 +0600

    Analytics tab Lead Conversion Funnel and Campaign ROI Distribution done

--- src/Components/General/Dashboard/OrganisationAdviser/MarketingHub/QuickGlance/AnalyticsTab/AnalyticsTab.tsx
+++ src/Components/General/Dashboard/OrganisationAdviser/MarketingHub/QuickGlance/AnalyticsTab/AnalyticsTab.tsx
@@ -1,7 +1,9 @@
 import { Col, Row } from "reactstrap";
+import LeadConversionFunnel from "./LeadConversionFunnel/LeadConversionFunnel";
 import MarketingAnalyticsOverview from "./MarketingAnalyticsOverview/MarketingAnalyticsOverview";
 import SocialMediaPerformance from "./SocialMediaPerformance/SocialMediaPerformance";
 import WhatsAppCampaignPerformance from "./WhatsAppCampaignPerformance/WhatsAppCampaignPerformance";
+import CampaignROIDistribution from "./CampaignROIDistribution/CampaignROIDistribution";
 
 const AnalyticsTab: React.FC = () => {
   return (
@@ -17,6 +19,14 @@ const AnalyticsTab: React.FC = () => {
         </Col>
         <Col md="6" sm="12">
           <WhatsAppCampaignPerformance />
+        </Col>
+      </Row>
+      <Row>
+        <Col md="6" sm="12">
+          <LeadConversionFunnel />
+        </Col>
+        <Col md="6" sm="12">
+          <CampaignROIDistribution />
         </Col>
       </Row>
     </>
---
+++ src/Components/General/Dashboard/OrganisationAdviser/MarketingHub/QuickGlance/AnalyticsTab/LeadConversionFunnel/LeadConversionFunnel.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Chart } from "react-google-charts";
+import { Card, CardBody } from "reactstrap";
+
+const data = [
+  ["Month", "Leads", "Qualified", "Approved", "Completed"],
+  ["Oct", 10, 4, 3, 12],
+  ["Nov", 14, 6, 5, 15],
+  ["Dec", 13, 7, 6, 14],
+];
+
+const options = {
+  isStacked: true,
+  chartArea: { width: "70%" },
+  colors: ["#c9b037", "#0081a7", "#00afb9", "#a259ff"],
+  legend: { position: "top" },
+  hAxis: {
+    title: "Month",
+  },
+  vAxis: {
+    title: "Count",
+    minValue: 0,
+  },
+  areaOpacity: 0.7,
+};
+
+const LeadConversionFunnel: React.FC = () => {
+  return (
+    <Card>
+      <CardBody>
+        <h2>Lead Conversion Funnel</h2>
+        <p>Track leads through to mortgage completions</p>
+        <Chart
+          chartType="AreaChart"
+          width="100%"
+          height="300px"
+          data={data}
+          options={options}
+        />
+      </CardBody>
+    </Card>
+  );
+};
+
+export default LeadConversionFunnel;
