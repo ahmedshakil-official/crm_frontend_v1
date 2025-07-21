686b5ae869d68dd9fe251ff536e22ba1af27c7e7
mahbub045 <mahbubrmn45@gmail.com>
2025-07-21 11:54:18 +0600

    Refactor client info

--- src/Types/CommonComponents/Directors/ClientTypes.tsx
+++ src/Types/CommonComponents/Directors/ClientTypes.tsx
@@ -1,7 +1,9 @@
 export interface ClientInfoProps {
   alias: string;
   user: {
+    title: String;
     first_name: string;
+    middle_name?: String;
     last_name: string;
     email?: string;
     phone?: string;
