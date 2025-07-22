1406046da1d72274139a9909d594505c9058a76e
mahbub045 <mahbubrmn45@gmail.com>
2025-07-22 15:43:50 +0600

    Internalcomment done

--- src/Components/General/Dashboard/OrganisationStaff/Comments/index.tsx
+++ src/Components/General/Dashboard/OrganisationStaff/Comments/index.tsx
@@ -1,18 +1,17 @@
 import { Container } from "reactstrap";
 import OrganisationStaffBreadcrumbs from "../Breadcrumbs/Breadcrumbs";
+import InternalComments from "./InternalComments/InternalComments";
 
 const index: React.FC = () => {
   return (
-     <>
+    <>
       <OrganisationStaffBreadcrumbs
-        mainTitle="Organisation Staff Dashboard"
+        mainTitle="Comments"
         title="Hello! there"
         activePage="Comments"
       />
       <Container fluid>
-        <div className="d-flex justify-content-center align-items-center h-100">
-          <h1 className="text-danger">This page is Under Development</h1>
-        </div>
+        <InternalComments />
       </Container>
     </>
   );
