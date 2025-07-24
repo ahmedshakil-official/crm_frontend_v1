d9b55aa033f11e642a872991eeb26b0c174f66db
mahbub045 <mahbubrmn45@gmail.com>
2025-07-24 10:38:32 +0600

    fix pie chart size

--- src/Components/General/Dashboard/Network/NetworkMain/Charts/LendersChart/LendersChart.tsx
+++ src/Components/General/Dashboard/Network/NetworkMain/Charts/LendersChart/LendersChart.tsx
@@ -6,7 +6,8 @@ import { Card, CardBody } from "reactstrap";
 const LendersChart: React.FC = () => {
   const PieChartData: ApexOptions = {
     chart: {
-      width: 380,
+      width: 450,
+      height: 280,
       type: "pie",
     },
     labels: [
@@ -17,6 +18,7 @@ const LendersChart: React.FC = () => {
       "Barclays",
       "HSBC",
       "Rightmove",
+      "Lloyds",
       "Others",
     ],
     series: [15, 13, 13, 12, 10, 8, 7, 22],
@@ -24,7 +26,7 @@ const LendersChart: React.FC = () => {
       {
         options: {
           chart: {
-            height: 280,
+            height: 300,
           },
           legend: {
             show: false,
@@ -41,19 +43,24 @@ const LendersChart: React.FC = () => {
       "#6366F1",
       "#EC4899",
       "#000000",
+      "#C70039",
     ],
   };
 
   return (
     <Card>
       <CommonCardHeader title="Lenders" />
-      <CardBody className="apex-chart">
-        <div id="piechart">
+      <CardBody
+        className="apex-chart"
+        style={{ width: "100%", maxWidth: "500px" }}
+      >
+        <div id="piechart" style={{ width: "100%", height: "280px" }}>
           <ReactApexChart
             options={PieChartData}
             series={PieChartData.series}
             type="pie"
-            width={380}
+            width="100%"
+            height={280}
           />
         </div>
       </CardBody>
