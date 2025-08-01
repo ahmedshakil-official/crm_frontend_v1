2291c732d536be534ee1d5a94bd22314129f71ef
mahbub045 <mahbubrmn45@gmail.com>
2025-08-01 16:53:53 +0600

    Update organisation links to use slugs and simplify email display

--- src/Components/General/Dashboard/Network/NetworkMain/OrganisationCards/OrganisationCards.tsx
+++ src/Components/General/Dashboard/Network/NetworkMain/OrganisationCards/OrganisationCards.tsx
@@ -79,7 +79,7 @@ const OrganisationCards = () => {
                 >
                   <Card className="bg-white border organisation_card opacity-100  p-3 position-relative">
                     <Link
-                      href={item?.website || "#"}
+                      href={`/dashboard/network/organisations/${item.slug}`}
                       target="_blank"
                       className="text-muted position-absolute top-0 end-0 p-3"
                     >
@@ -109,15 +109,7 @@ const OrganisationCards = () => {
                           </Link>
                         </h5>
                       </div>
-                      <div className="mt-2">
-                        <Link
-                          href={`mailto:${item.email}`}
-                          className="fs-6 text_decoration_hover mb-2"
-                        >
-                          <small> {item.email}</small>
-                        </Link>
-                      </div>
-
+                      <div className="mt-2 mb-4">{item.email}</div>
                       <div className="d-flex justify-content-between mt-3 pt-2 border-top">
                         <Col className="border-end">
                           <div className="text-center ">
