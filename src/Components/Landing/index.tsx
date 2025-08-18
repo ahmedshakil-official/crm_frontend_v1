8ae64ac192707bdc7c6ca80a31c53a6efad37f6e
mahbub045 <mahbubrmn45@gmail.com>
2025-08-18 13:03:26 +0600

    Make Footer

--- src/Components/Landing/index.tsx
+++ src/Components/Landing/index.tsx
@@ -1,17 +1,19 @@
 import { Container } from "reactstrap";
 import UnderDevelopment from "../Other/UnderDevelopment/UnderDevelopment";
+import LandingFooter from "./Components/Footer/LandingFooter";
 import NavBar from "./Components/NavBar/NavBar";
 import "./LandingPageStyle.css";
 
 const LandingContainer: React.FC = () => {
   return (
     <>
+      <NavBar />
       <main>
-        <NavBar />
+        <Container fluid>
+          <UnderDevelopment />
+        </Container>
       </main>
-      <Container fluid>
-        <UnderDevelopment />
-      </Container>
+      <LandingFooter />
     </>
   );
 };
